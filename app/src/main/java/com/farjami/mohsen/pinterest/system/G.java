de70802dc3414baba8e21bf1c123e471d09c4ae1
unknown <mohsenfarjami340@gmail.com>
2019-04-16 15:03:44 +0430

    change bottom bar icons

--- app/src/main/java/com/farjami/mohsen/pinterest/system/G.java
+++ app/src/main/java/com/farjami/mohsen/pinterest/system/G.java
@@ -25,7 +25,7 @@ public class G extends Application {
   public static Context context;
 //  public static final String MAIN_URL="http://lifemo.ud/api";
 //  public static final String MAIN_URL="http://ezitech.ir/lifemo/public/api";
-  public static final String MAIN_URL="http://192.168.1.9/lifemo/api";
+  public static final String MAIN_URL="http://192.168.1.6/lifemo/api";
   public static final String SALT="7c3d596ed03ab9116c547b0eb678b247";
 
   public static final String SALT_COPY="7c3d596ed03ab9116c547b0eb678b247";
